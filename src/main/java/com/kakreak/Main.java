17d58628a6b71a98afe4e69e634405de9c4323db
kak_reak <timberr.4t@gmail.com>
2023-05-31 03:03:01 +0700

    🚀 feat: add `favProgrammingLanguages` and `person` fields to `GreetResponse` record
    0026_Java_Objects_to_JSON_Objects

--- src/main/java/com/kakreak/Main.java
+++ src/main/java/com/kakreak/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -14,8 +16,20 @@ public class Main {
 
     @GetMapping("/")
     public GreetResponse greet() {
-        return new GreetResponse("Hello World");
+        GreetResponse response = new GreetResponse(
+                "Hello World",
+                List.of("Java", "Kotlin", "Scala"),
+                new Person("Kakreak", 20, 100.0));
+        return response;
     }
 
-    record GreetResponse(String greet) {}
+    record Person(String name, int age, double savings) {
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ) {
+    }
 }
