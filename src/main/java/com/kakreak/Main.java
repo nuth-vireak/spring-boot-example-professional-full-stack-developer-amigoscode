a1782131a3e1902f923d34737dbc614244feee40
kak_reak <timberr.4t@gmail.com>
2023-06-01 18:37:22 +0700

    ✨ feat(application): update server port to 8080
    🐛 fix(main): add optional name parameter to greet endpoint
    
    0032_URL--[TutFlix.ORG]--

--- src/main/java/com/kakreak/Main.java
+++ src/main/java/com/kakreak/Main.java
@@ -3,6 +3,7 @@ package com.kakreak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,9 +16,11 @@ public class Main {
     }
 
     @GetMapping("/")
-    public GreetResponse greet() {
+    public GreetResponse greet(@RequestParam(value = "name", required = false) String name) {
+
+        String greetMessage = name == null || name.isBlank() ? "Hello World" : "Hello " + name;
         GreetResponse response = new GreetResponse(
-                "Hello World",
+                greetMessage,
                 List.of("Java", "Kotlin", "Scala"),
                 new Person("Kakreak", 20, 100.0));
         return response;
