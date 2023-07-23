fdfb28978e132fe6ff2edd6193e408fe33a630ab
nuth-vireak <timberr.4t@gmail.com>
2023-07-24 00:32:49 +0700

    add

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # spring-boot-example-professional-full-stack-developer-amigoscode
 
 ## Database progress : `38:13`
-## SpringBoot progress: `0125_Testcontainers`
+## SpringBoot progress: `0129_Inspecting_DB_created_by_Testcontainers`
---
+++ src/test/java/com/kakreak/TestcontainersTest.java
@@ -0,0 +1,29 @@
+package com.kakreak;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("amigoscode-dao-unit-test")
+                    .withUsername("postgres")
+                    .withPassword("1234");
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        assertThat(postgreSQLContainer.isHealthy()).isTrue();
+    }
+}
