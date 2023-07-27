ebd63b2ac82a8bf0a2033b53e882947677777143
nuth-vireak <timberr.4t@gmail.com>
2023-07-27 15:16:55 +0700

    Abstract_Testcontainres_Logic

---
+++ src/test/java/com/kakreak/AbstractTestContainers.java
@@ -0,0 +1,39 @@
+package com.kakreak;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractTestContainers {
+
+    @BeforeAll
+    static void beforeAll() {
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+    }
+
+    @Container
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("amigoscode-dao-unit-test")
+                    .withUsername("postgres")
+                    .withPassword("1234");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+}
--- src/test/java/com/kakreak/TestcontainersTest.java
+++ src/test/java/com/kakreak/TestcontainersTest.java
@@ -1,40 +1,14 @@
 package com.kakreak;
 
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-public class TestcontainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("amigoscode-dao-unit-test")
-                    .withUsername("postgres")
-                    .withPassword("1234");
+public class TestcontainersTest extends AbstractTestContainers {
 
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-    }
-
-    @Test
-    void canApplyDbMigrationWithFlyway() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
-        flyway.migrate();
-        System.out.println();
     }
 }
